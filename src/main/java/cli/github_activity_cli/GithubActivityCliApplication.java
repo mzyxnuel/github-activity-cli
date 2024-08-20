974a7e451879d6d9607e90fdbe8c21c3681b4b19
mzyxnuel <137792640+mzyxnuel@users.noreply.github.com>
2024-08-20 14:51:31 +0100

    Initial commit

---
+++ src/main/java/cli/github_activity_cli/GithubActivityCliApplication.java
@@ -0,0 +1,13 @@
+package cli.github_activity_cli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubActivityCliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubActivityCliApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cli/github_activity_cli/GithubActivityCliApplicationTests.java
@@ -0,0 +1,13 @@
+package cli.github_activity_cli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActivityCliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
